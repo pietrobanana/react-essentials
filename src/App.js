4d610c8f5aaa2c0a2c6933ce9c5ec602199501d9
jpietrogiovanna <jpietrogiovanna@aafp.org>
2021-10-06 13:47:28 -0500

    introducing tests

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState, useEffect} from 'react';
 
-function App() {
+// https://api.github.com/users/pietrobanana
+
+function App({ login }) {
+  const [data, setData] = useState(null);
+  const[loading, setLoading] = useState(null);
+  const [error, setError] = useState(null);
+
+  useEffect(() => {
+    if(!login) return;
+    setLoading(true);
+    fetch(`https://api.github.com/users/${login}`)
+      .then((response) => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]);
+
+  if(loading) return <h1>loading...</h1>;
+  if(error) 
+    return <pre>{JSON.stringify(error, null, 2)}</pre>
+  if(!data) return null;
+    
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div>
+        <h1>{data.login}</h1>
+        <p>{data.id}</p>
+        <img atl={data.login} src={data.avatar_url} />
+      </div>
   );
 }
 
