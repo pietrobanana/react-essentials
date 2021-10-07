44d9814e884d877eb478963935eb41c6b617d536
jpietrogiovanna <jpietrogiovanna@aafp.org>
2021-10-07 10:33:02 -0500

    implemented routing

--- src/App.js
+++ src/App.js
@@ -1,33 +1,31 @@
+import React from 'react';
 import './App.css';
-import React, {useState, useEffect} from 'react';
+import {Routes, Route} from "react-router-dom";
+import {
+  Home,
+  About,
+  Events,
+  Contact, 
+  Whoops404,
+  Services,
+  CompanyHistory,
+  Location
+} from "./pages"
 
-// https://api.github.com/users/pietrobanana
-
-function App({ login }) {
-  const [data, setData] = useState(null);
-  const[loading, setLoading] = useState(null);
-  const [error, setError] = useState(null);
-
-  useEffect(() => {
-    if(!login) return;
-    setLoading(true);
-    fetch(`https://api.github.com/users/${login}`)
-      .then((response) => response.json())
-      .then(setData)
-      .then(() => setLoading(false))
-      .catch(setError);
-  }, [login]);
-
-  if(loading) return <h1>loading...</h1>;
-  if(error) 
-    return <pre>{JSON.stringify(error, null, 2)}</pre>
-  if(!data) return null;
-    
+function App() {
   return (
       <div>
-        <h1>{data.login}</h1>
-        <p>{data.id}</p>
-        <img atl={data.login} src={data.avatar_url} />
+        <Routes>
+          <Route path="/" element={<Home />}/>
+          <Route path="/about" element={<About />}>
+            <Route path="services" element={<Services />}/>
+            <Route path="history" element={<CompanyHistory />}/>
+            <Route path="location" element={<Location />}/>
+          </Route>
+          <Route path="/events" element={<Events />}/>
+          <Route path="/contact" element={<Contact />}/>
+          <Route path="*" element={<Whoops404 />}/>
+        </Routes>
       </div>
   );
 }
---
+++ src/Checkbox.js
@@ -0,0 +1,21 @@
+import React from "react"
+
+export function Checkbox() {
+    const [checked, toggle] = useReducer(
+        (checked) => !checked,
+        false
+    );
+    return (
+        <>
+            <label htmlFor="checkbox">
+                {checked ? "checked" : "not checked"}
+            </label>
+            <input 
+                id="checkbox"
+                type="checkbox" 
+                value={checked} 
+                onChange={toggle}/>
+        </>
+    )
+    
+}
